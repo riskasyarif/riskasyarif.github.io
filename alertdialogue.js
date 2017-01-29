76b407de2fb6e81374763b8905d9ad6a0926cf09
riskasyarif <riska.syarif@gmail.com>
2017-01-29 18:32:45 +0700

    Add files via upload

---
+++ alertdialogue.js
@@ -0,0 +1,9 @@
+function Warn() {
+    alert ("This is a warning message!");
+    document.write ("This is a warning msg!")
+}
+
+function Warn1() {
+    alert ("This is a warning sign!");
+    document.write ("This is a warning sign!")
+}
